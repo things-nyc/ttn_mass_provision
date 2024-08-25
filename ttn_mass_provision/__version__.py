e3fe603012888cfa91414488a582c3202cd95d6f
Terry Moore <tmm@mcci.com>
2024-08-25 19:01:00 -0400

    This is version 0.9.0-pre3

--- ttn_mass_provision/__version__.py
+++ ttn_mass_provision/__version__.py
@@ -14,4 +14,4 @@
 ##############################################################################
 
 # this is the version -- setup.cfg uses it, as does __init__.
-__version__ = "0.9.0-pre2"
+__version__ = "0.9.0-pre3"
